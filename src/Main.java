8a1d2dde3c8628163644294bf02ddfcfb2cffda9
Aleksey Grushenkov <153802645+Grugrusha96@users.noreply.github.com>
2024-01-05 22:21:06 +0300

    min-max

--- src/Main.java
+++ src/Main.java
@@ -9,51 +9,107 @@
 //        }
 //    }
 //}
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int[] array = {10, 12, 14, 16, 18};
+//        System.out.print("Массив изначально: ");
+//        System.out.println(Arrays.toString(array));
+//
+//        System.out.print("\nчисло которое хотим удалить: ");
+//        int target = scanner.nextInt();
+//
+//        if (!searcher(array, target)) {
+//            System.out.println("Такого числа нет!");
+//        } else {
+//            removeElement(array, target);
+//
+//            System.out.println(Arrays.toString(array));
+//        }
+//
+//    }
+//
+//
+//
+//
+//    private static boolean searcher(int array[], int target){
+//        for (int number : array) {
+//            if (number == target) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    private static int[] removeElement(int array[], int target) {
+//        int number = 0;
+//        for (int readNumber = 0; readNumber < array.length; readNumber++) {
+//            if (array[readNumber] != target) {
+//                array[number++] = array[readNumber];
+//            }
+//        }
+//        while (number < array.length) {
+//            array[number++] = 0;
+//        }
+//        return array;
+//
+//    }
+//}
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] array = {10, 12, 14, 16, 18};
-        System.out.print("Массив изначально: ");
-        System.out.println(Arrays.toString(array));
 
-        System.out.print("\nчисло которое хотим удалить: ");
-        int target = scanner.nextInt();
-
-        if (!searcher(array, target)) {
-            System.out.println("Такого числа нет!");
-        } else {
-            removeElement(array, target);
-
-            System.out.println(Arrays.toString(array));
+        System.out.print("размер массива: ");
+        if (!scanner.hasNextInt()) {
+            System.out.println("Размер массива должен быть целым числом.");
+            return;
         }
 
-    }
+        int size = scanner.nextInt();
+        if (size <= 0) {
+            System.out.println("Размер массива должен быть положительным числом.");
+            return;
+        }
 
+        int[] array = new int[size];
 
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(101) - 50;
+        }
 
+        System.out.println("Массив: " + Arrays.toString(array));
 
-    private static boolean searcher(int array[], int target){
-        for (int number : array) {
-            if (number == target) {
-                return true;
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
             }
         }
-        return false;
-    }
-    private static int[] removeElement(int array[], int target) {
-        int number = 0;
-        for (int readNumber = 0; readNumber < array.length; readNumber++) {
-            if (array[readNumber] != target) {
-                array[number++] = array[readNumber];
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
             }
         }
-        while (number < array.length) {
-            array[number++] = 0;
+
+
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
         }
-        return array;
+        double average = (double) sum / array.length;
+
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Максимальное значение: " + max);
+        System.out.println("Среднее значение: " + average);
 
     }
 }
