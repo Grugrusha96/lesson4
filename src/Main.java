88768bb502b90e21cfa6f62f3ea0f3242304c159
Aleksey Grushenkov <153802645+Grugrusha96@users.noreply.github.com>
2024-01-05 17:35:13 +0300

    array of integers

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java. util. Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[4];
+        for (int i = 0; 1 < 4; i++) {
+            array[i] = scanner.nextInt();
+        }
+    }
+}
