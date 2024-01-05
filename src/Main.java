bf5767c6f7c0813900d96d6dd37826c3e4c5dbc3
Aleksey Grushenkov <153802645+Grugrusha96@users.noreply.github.com>
2024-01-05 18:01:12 +0300

    deleting a number

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,63 @@
-import java. util. Scanner;
+//import java. util. Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int[] array = new int[4];
+//        for (int i = 0; 1 < 4; i++) {
+//            array[i] = scanner.nextInt();
+//        }
+//    }
+//}
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] array = new int[4];
-        for (int i = 0; 1 < 4; i++) {
-            array[i] = scanner.nextInt();
+        int[] array = {10, 12, 14, 16, 18};
+        System.out.print("Массив изначально: ");
+        System.out.println(Arrays.toString(array));
+
+        System.out.print("\nчисло которое хотим удалить: ");
+        int target = scanner.nextInt();
+
+        if (!searcher(array, target)) {
+            System.out.println("Такого числа нет!");
+        } else {
+            removeElement(array, target);
+
+            System.out.println(Arrays.toString(array));
         }
+
+    }
+
+
+
+
+    private static boolean searcher(int array[], int target){
+        for (int number : array) {
+            if (number == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static int[] removeElement(int array[], int target) {
+        int number = 0;
+        for (int readNumber = 0; readNumber < array.length; readNumber++) {
+            if (array[readNumber] != target) {
+                array[number++] = array[readNumber];
+            }
+        }
+        while (number < array.length) {
+            array[number++] = 0;
+        }
+        return array;
+
     }
 }
+
+
+
+
